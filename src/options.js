349e6d71984f92894f59c4be10a4ed1b0d955153
Tommy Brunn <tommy.brunn@gmail.com>
2018-05-29 15:28:08 +0200

    Remove experimental flag

--- src/options.js
+++ src/options.js
@@ -2,8 +2,7 @@ const uuidv4 = require("uuid/v4");
 
 const DEFAULT_OPTIONS = {
   maxByteSize: 1500,
-  createReference: () => `Log-Reference-${uuidv4()}`,
-  useExperimental: false
+  createReference: () => `Log-Reference-${uuidv4()}`
 };
 
 module.exports = configuration =>
